6550f26c0e2753cdad6d655494f8c2e034851aab
Bibi <bibiaremieye@gmail.com>
2021-06-26 00:12:34 +0100

    click Got it goto home page

--- src/components/CheckoutSuccess/CheckoutSuccess.tsx
+++ src/components/CheckoutSuccess/CheckoutSuccess.tsx
@@ -3,8 +3,10 @@ import React from "react";
 import checkout from "assets/images/checkout.svg";
 
 import "./style.css";
+import { useHistory } from "react-router-dom";
 
 const CheckoutSuccess = () => {
+  const history = useHistory();
   return (
     <div className="checkout-screen">
       <div></div>
@@ -13,7 +15,7 @@ const CheckoutSuccess = () => {
         <h3>Checkout Succesful</h3>
         <span>Your checkout is now successful.</span>
       </div>
-      <button>Got It</button>
+      <button onClick={() => history.push("/")}>Got It</button>
     </div>
   );
 };
